6c4817cbd98829a76f1a8240559a1eb690fb2c04
rbilash <rbilash@udel.edu>
2022-10-14 12:48:33 -0400

    Added main functionality and import functionality

--- stream_collector.py
+++ stream_collector.py
@@ -2,6 +2,8 @@ import csv
 from pylsl import *
 import time
 import datetime
+import threading
+import atexit
 
 
 class stream_collector:
@@ -17,6 +19,8 @@ class stream_collector:
         self.cached_stream_rate = self.stream_rate()
         self.cached_time_correction = self.time_correction()
         self.data = []
+        self.running = True
+        # self.resample_time_correction_rate = None # Unused method of regathering the time correction
 
     def collect(self, chunk_size=1):
         if chunk_size == 1:
@@ -51,3 +55,59 @@ class stream_collector:
         output_file.close()
 
         return filename
+
+
+streams = []
+
+
+def listening_thread(stream_name, keep_searching=False, chunk_size=1, log_data=False):
+    stream = stream_collector(stream_name, keep_searching)
+    streams.append(stream)
+
+    while stream.running:
+        data, timestamps = stream.collect(chunk_size)
+        if log_data:
+            print(data, timestamps)
+    stream.output_csv()
+
+
+def read_file(filename):
+    file = open(filename, 'r')
+    lines = file.readlines()
+    for line in lines:
+        listener_args = None
+        try:
+            # Split at ; for the name,  and other optional settings
+            split_line = line.split(";")
+            split_line[1] = split_line[1].replace(" ", "")
+            listener_args = [split_line[0], split_line[1].lower().startswith('t')]
+
+            # Get chunk size
+            if len(split_line) > 2:
+                split_line[2] = split_line[2].replace(" ", "")
+                listener_args.append(int(split_line[2]))
+
+            # Get whether or not data should be logged to console
+            if len(split_line) > 3:
+                split_line[3] = split_line[3].replace(" ", "")
+                listener_args.append(split_line[3].lower().startswith('t'))
+        except:
+            print('Error with line "' + line + '"')
+            continue
+
+        if listener_args is not None:
+            thread = threading.Thread(target=listening_thread, args=listener_args)
+            thread.start()
+            print(listener_args[0] + " thread started")
+
+    file.close()
+
+
+def exit_handler():
+    for stream in streams:
+        stream.running = False
+
+
+if __name__ == "__main__":
+    in_filename = input("Input filepath for file with stream names: ")
+    atexit.register(exit_handler)
